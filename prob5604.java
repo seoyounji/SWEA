a4431233086a5df72a7de363f2867f80b69bf52c
seoyounji <wltjdus0208@naver.com>
2020-12-06 10:00:00 +0900

    SWEA 5604 구간 합

---
+++ prob5604.java
@@ -0,0 +1,22 @@
+package ComputingThinking;
+
+import java.util.Scanner;
+
+//삼성 5604 [Professional] 구간 합
+public class prob5604 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test_case = sc.nextInt();
+		for (int i = 1; i <= test_case; i++) {
+			int result = 0;
+			long A = sc.nextLong();
+			long B = sc.nextLong();
+			int index = 0;
+			
+			
+			
+			System.out.println("#"+i+" "+result);
+		}
+	}
+}
